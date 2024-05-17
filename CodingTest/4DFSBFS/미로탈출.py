45ddd67a7a0cbf17b985de2edb5025188cd5e502
joongwonAn <165457428+joongwonAn@users.noreply.github.com>
2024-05-17 08:58:25 +0000

    20240517

---
+++ CodingTest/4DFSBFS/미로탈출.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+  graph.append(list(map(int, input())))
+
+# 이동할 네 방향 정의
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y):
+  queue = deque()
+  queue.append((x, y))
+  while queue:
+    x, y = queue.popleft()
+    # 현재 위치에서 네 방향으로의 위치 확인
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if nx < 0 or nx >= n or ny < 0 or ny >= m:
+        continue
+
+
+result = 0
+for i in range(n):
+  for j in range(m):
+    if bfs(i, j) == True:
+      result += 1
+
+print(result)
