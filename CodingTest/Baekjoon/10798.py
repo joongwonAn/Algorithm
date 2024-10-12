9035c5a63647854822411a5e2d88262a0bb10096
joongwon <sara970517@gmail.com>
2024-10-12 13:48:42 +0900

    *리스트 컴프리헨션

--- CodingTest/Baekjoon/10798.py
+++ CodingTest/Baekjoon/10798.py
@@ -1,13 +1,12 @@
-table = []
-for _ in range(5):
-  table.append(list(map(int, input().split())))
+# words = []
+# for _ in range(5):
+#   a = input()
+#   words.append(a)
 
-# for i in range(5):
-#   if(len(table[i]) < 16):
-#     for _ in range(15 - len(table[i])):
-#       table[i].append(None)
+# 리스트 컴프리헨션(list comprehension): 리스트를 만드는 간결한 방법 제공
+words = [input() for _ in range(5)]
 
-for i in range(15):
-  for j in range(5):
-    if (table[j][i] is not None):
-      print(table[j][i], end = "")
+for j in range(15):
+  for i in range(5):
+    if (j < len(words[i])):
+      print(words[i][j], end ='')
