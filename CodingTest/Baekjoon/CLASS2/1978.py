74ec07ca86fbef6101d74f7e002fde176939d500
joongwon <sara970517@gmail.com>
2024-10-19 21:10:20 +0900

    소수 정의

---
+++ CodingTest/Baekjoon/CLASS2/1978.py
@@ -0,0 +1,14 @@
+n = int(input())
+nums = list(map(int, input().split()))
+result = 0
+
+for num in nums:
+    count = 0
+    for i in range(1,num+1):
+        if num % i == 0:
+            count += 1
+
+    if count == 2:
+        result += 1
+
+print(result)
