65bcb667dcaa1e6567e5447b6b057ebbc2205e88
joongwon <sara970517@gmail.com>
2024-10-27 15:36:58 +0900

    블랙잭, combinations

---
+++ CodingTest/Baekjoon/CLASS2/15829.py
@@ -0,0 +1,14 @@
+# 입력
+len = int(input())
+userList = input()
+
+M = 1234567891
+R = 31
+
+# ord('a') = 97 이용하여 알파벳에 고유한 계수 부여 (1~26)
+hashResult = 0
+for i in range(len):
+    num = ord(userList[i]) - 96
+    hashResult += num * (R ** i)
+
+print(hashResult % M)
